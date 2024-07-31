580f7071afdd4b86183a1dbb04b512f4ceeca2b9
LCottin <cottin_leonard@icloud.com>
2024-07-31 22:45:07 +0200

    Updated main to use functions

--- dev/main.c
+++ dev/main.c
@@ -1,30 +1,19 @@
-#define QEMU_UART_ADDR 0x09000000
+#include "kernelPrint.h"
 
-void main()
+void memcpy(void *dest, const void *src, int size)
 {
-    *((volatile unsigned int *)QEMU_UART_ADDR) = '[';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'M';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'A';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'I';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'N';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = ']';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = ' ';
+    unsigned char *d       = (unsigned char *)dest;
+    const unsigned char *s = (const unsigned char *)src;
 
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'H';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'e';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'l';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'l';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'o';
+    while(size--)
+        *d++ = *s++;
+}
 
-    *((volatile unsigned int *)QEMU_UART_ADDR) = ' ';
+void main(void)
+{
+    const char str[64] = "[MAIN] Hello World!\n";
+    kernelPrint(str);
 
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'w';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'o';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'r';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'l';
-    *((volatile unsigned int *)QEMU_UART_ADDR) = 'd';
-
-    *((volatile unsigned int *)QEMU_UART_ADDR) = '!';
-
+    kernelPrint("[MAIN] Going to infinite loop ... ");
     while(1) ;
 }
