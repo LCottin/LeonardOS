6a020fdbe80db2909243e91d78b0f9e81f50f3dd
LCottin <cottin_leonard@icloud.com>
2024-09-03 19:00:44 +0200

    Add new function to lib

--- dev/common/memory/include/memory.h
+++ dev/common/memory/include/memory.h
@@ -11,8 +11,23 @@
  * @param src Pointer to the source memory area from where the content is to be copied.
  * @param size Number of bytes to be copied.
  *
- * @return None.
+ * @return Pointer to the destination memory area.
  */
-extern void memcpy(void *dest, const void *src, const int size);
+extern void *memcpy(void *dest, const void *src, const int size);
 
-#endif
+/**
+ * @brief Fills the memory block pointed to by 'dest' with the specified 'value'.
+ *
+ * The function sets the first 'size' bytes of the memory block pointed to by 'dest' to the specified 'value'.
+ * The function does not check for any overlapping areas between the source and destination memory blocks.
+ * If the source and destination memory blocks overlap, the behavior of this function is undefined.
+ *
+ * @param dest Pointer to the destination memory area where the content is to be filled.
+ * @param value The value to be set in the memory block. The value is passed as an integer, but it will be interpreted as unsigned char.
+ * @param size Number of bytes to be filled with the specified 'value'.
+ *
+ * @return Pointer to the destination memory area.
+ */
+extern void *memset(void *dest, const int value, const int size);
+
+#endif /* __MEMORY_H__ */
--- dev/common/memory/src/memory.c
+++ dev/common/memory/src/memory.c
@@ -1,6 +1,6 @@
 #include "memory.h"
 
-void memcpy(void *dest, const void *src, const int size)
+void *memcpy(void *dest, const void *src, const int size)
 {
     unsigned char *d       = (unsigned char *)dest;
     const unsigned char *s = (const unsigned char *)src;
@@ -10,4 +10,19 @@ void memcpy(void *dest, const void *src, const int size)
     {
         *d++ = *s++;
     }
+
+    return dest;
+}
+
+void *memset(void *dest, const int value, const int size)
+{
+    unsigned char *d = (unsigned char *)dest;
+    int sizeToFill   = size;
+
+    while(sizeToFill--)
+    {
+        *d++ = (unsigned char)value;
+    }
+
+    return dest;
 }
