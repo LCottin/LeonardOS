4bddf96af32452ff5e8ae6edca764538b7d51032
LCottin <cottin_leonard@icloud.com>
2024-09-18 14:26:57 +0200

    Hot-fix stdlib

--- dev/common/stdlib/src/stdlib.c
+++ dev/common/stdlib/src/stdlib.c
@@ -1,24 +1,27 @@
 #include "string.h"
 
 /* Reverse a string */
-static void reverseString(char *str, const int length)
+static void reverseString(int8_t *str, const size_t length)
 {
-    int start = 0;
-    int end   = length - 1;
-
-    while (start < end)
+    if (length > 0)
     {
-        char temp  = str[start];
-        str[start] = str[end];
-        str[end]   = temp;
-        start++;
-        end--;
+        uint32_t start = 0;
+        uint32_t end   = length - 1;
+
+        while (start < end)
+        {
+            char temp  = str[start];
+            str[start] = str[end];
+            str[end]   = temp;
+            start++;
+            end--;
+        }
     }
 }
 
-char *itoa(const int num, char *str, const int base)
+int8_t *itoa(const int32_t num, int8_t *str, const int32_t base)
 {
-    char *strToReturn;
+    int8_t *strToReturn;
 
     /* Check for base between 2 (binary) and 16 (hexadecimal) */
     if ((base < 2) || (base > 16))
@@ -28,9 +31,9 @@ char *itoa(const int num, char *str, const int base)
     }
     else
     {
-        int isNegative;
-        int number;
-        int length = 0;
+        uint32_t isNegative;
+        int32_t  number;
+        size_t   length = 0;
 
         /* Handle negative number in decimal base */
         if ((num < 0) && (base == 10))
@@ -47,7 +50,7 @@ char *itoa(const int num, char *str, const int base)
         /* Process digit to convert to string */
         do
         {
-            int remainder = number % base;
+            int32_t remainder = number % base;
             str[length++] = (remainder > 9) ? (remainder - 10) + 'a' : remainder + '0';
             number /= base;
         } while (number != 0);
