6a3a095473358e16ba4b9d832707cd54f49a15a0
LCottin <cottin_leonard@icloud.com>
2024-11-16 17:27:42 +0100

    Update UART to use MMIO

--- dev/kernel/core/UART/src/UART.c
+++ dev/kernel/core/UART/src/UART.c
@@ -1,7 +1,8 @@
 #include "UART_prv.h"
 #include "UART.h"
+#include "MMIO_io8.h"
 
 void UART_write_byte(const char_t c)
 {
-    *((volatile uint32_t *)K_UART_QEMU_VIRT_ADDR) = c;
+    mmio_io8_write((cptr_t)K_UART_QEMU_VIRT_ADDR, c);
 }
