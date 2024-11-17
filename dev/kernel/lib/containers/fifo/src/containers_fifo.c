6b0d59ed8cfae6139e5b7b8af60d08ff242b59b5
LCottin <cottin_leonard@icloud.com>
2024-11-17 17:57:47 +0100

    Creation of container fifo

---
+++ dev/kernel/lib/containers/fifo/src/containers_fifo.c
@@ -0,0 +1,84 @@
+#include "containers_fifo_lib.h"
+#include "memory_alloc_lib.h"
+
+void containers_fifo_init(containers_fifo_t *queue)
+{
+    queue->head = NULL;
+    queue->tail = NULL;
+    queue->size = 0;
+}
+
+bool_t containers_fifo_is_empty(const containers_fifo_t *queue)
+{
+    return (bool_t)(queue->size == 0);
+}
+
+ptr_t containers_fifo_enqueue(containers_fifo_t *queue, ptr_t data)
+{
+    ptr_t ptr_to_return = NULL;
+
+    if (queue != NULL)
+    {
+        /* Allocate a new node to insert in fifo */
+        containers_fifo_node_t *new_node = (containers_fifo_node_t *)memory_alloc(sizeof(containers_fifo_node_t));
+
+        if (new_node != NULL)
+        {
+            new_node->data = data;
+            new_node->next = NULL;
+
+            if (queue->size == 0)
+            {
+                queue->head = new_node;
+            }
+            else
+            {
+                queue->tail->next = new_node;
+            }
+
+            queue->tail = new_node;
+            queue->size++;
+            ptr_to_return = new_node;
+        }
+    }
+
+    return ptr_to_return;
+}
+
+ptr_t containers_fifo_dequeue(containers_fifo_t *queue)
+{
+    ptr_t ptr_to_return = NULL;
+
+    if ((queue != NULL) && (queue->size > 0))
+    {
+        /* Get the node to free */
+        containers_fifo_node_t *node_to_free = queue->head;
+
+        /* Update the head of the queue */
+        queue->head = node_to_free->next;
+
+        if (queue->size == 1)
+        {
+            queue->tail = NULL;
+        }
+
+        /* Free the node */
+        memory_free(node_to_free);
+
+        queue->size--;
+        ptr_to_return = queue->head;
+    }
+
+    return ptr_to_return;
+}
+
+void containers_fifo_destroy(containers_fifo_t *queue)
+{
+    if (queue != NULL)
+    {
+        while (queue->size > 0)
+        {
+            containers_fifo_dequeue(queue);
+        }
+    }
+}
