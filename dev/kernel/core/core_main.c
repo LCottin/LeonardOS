cd71e4c9d6c10aae04b2971a4c9affe0312c164b
LCottin <cottin_leonard@icloud.com>
2025-01-02 22:25:58 +0100

    Add missing comments

--- dev/kernel/core/core_main.c
+++ dev/kernel/core/core_main.c
@@ -4,28 +4,34 @@
 #include "memory_ops.h"
 #include "memory_alloc.h"
 #include "FPU.h"
+#include "clock.h"
 
 void core_main(void)
 {
     memory_init_heap();
     FPU_init();
+    clock_init();
 
-    const char_t str[64] = "[MAIN] Hello World!\n";
+    const char_t str[64] = "\n[MAIN] Hello World!\n";
+    char_t str2[64];
 
     serial_print_string(str);
 
     char_t *ptr_alloc = (char_t *)memory_alloc_and_init(sizeof(char_t) * 64, 0);
     if (ptr_alloc != NULL)
     {
-        memory_ops_copy(ptr_alloc, "[MAIN] Hello World from allocated buffer!\n", 64);
+        memory_ops_copy(ptr_alloc, "\n[MAIN] Hello World from allocated buffer!\n", 64);
         serial_print_string(ptr_alloc);
         memory_free(ptr_alloc);
     }
     else
     {
-        serial_print_string("[MAIN] Allocate memory failed\n");
+        serial_print_string("\n[MAIN] Allocate memory failed\n");
     }
 
-    serial_print_string("[MAIN] Going to infinite loop ... ");
+    serial_print_string("\n[MAIN] Current elapsed time (ns) : ");
+    serial_print_string(string_utils_itoa((int32_t)clock_get_time(), str2, 10U));
+
+    serial_print_string("\n[MAIN] Going to infinite loop ... ");
     while(1) ;
 }
--- dev/kernel/lib_memory/alloc/src/memory_alloc_prv.h
+++ dev/kernel/lib_memory/alloc/src/memory_alloc_prv.h
@@ -4,22 +4,41 @@
 #include "memory_alloc.h"
 #include "types.h"
 
+/**
+ * @brief Size of a memory block structure.
+ */
 #define MEMORY_BLOCK_SIZE (sizeof(memory_block_t))
 
+/**
+ * @brief Aligns a size to the nearest memory alignment boundary.
+ *
+ * @param size The size to align.
+ * @return The aligned size.
+ */
 #define MEMORY_ALIGN(size) (((size) + (MEMORY_ALIGNMENT - 1)) & ~(MEMORY_ALIGNMENT - 1))
 
 /**
- * @brief Defines the memory block
+ * @brief Defines the memory block structure.
  *
+ * This structure represents a block of memory in the heap. It contains
+ * the size of the block, a flag indicating whether the block is free,
+ * and a pointer to the next block in the heap.
  */
 typedef struct memory_block
 {
-    size_t size;
-    bool_t is_free;
-    struct memory_block *next;
+    size_t size;               /* Size of the memory block */
+    bool_t is_free;            /* Flag indicating if the block is free */
+    struct memory_block *next; /* Pointer to the next memory block */
 } memory_block_t;
 
+/**
+ * @brief Start address of the heap.
+ */
 extern addr_t __heap_start;
+
+/**
+ * @brief Size of the heap.
+ */
 extern addr_t __heap_size;
 
 #endif /* __MEMORY_ALLOC_PRV_H__ */
