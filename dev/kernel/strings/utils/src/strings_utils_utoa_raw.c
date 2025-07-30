893fb87c0b95c348c641c0194298cfd9a2f979fc
LCottin <cottin_leonard@icloud.com>
2025-07-30 11:03:36 +0200

    Improve strings_utils with new functions

---
+++ dev/kernel/strings/utils/src/strings_utils_utoa_raw.c
@@ -0,0 +1,25 @@
+#include "strings_utils_prv.h"
+
+size_t strings_utils_utoa_raw(const printer_base_t base, const uint64_t value, char_t *buffer)
+{
+    const char_t digits[17] = "0123456789abcdef";
+    size_t length           = 0;
+    uint64_t raw_value      = value;
+
+    if (raw_value == 0)
+    {
+        buffer[length++] = '0';
+    }
+    else
+    {
+        /* Nothing to do */
+    }
+
+    while (raw_value > 0)
+    {
+        buffer[length++] = digits[raw_value % base];
+        raw_value /= base;
+    }
+
+    return length;
+}
