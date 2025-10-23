0bd36a61c54aa338ff9d533506dfe66140c1ac21
LCottin <cottin_leonard@icloud.com>
2025-10-23 19:05:40 +0200

    Rename assembly files

--- dev/kernel/clock/info/src/clock_info_get_time.c
+++ dev/kernel/clock/info/src/clock_info_get_time.c
@@ -6,7 +6,7 @@ time_t clock_info_get_time()
     uint64_t current_cycles = clock_info_get_cycle_count();
     uint64_t frequency      = clock_info_get_frequency();
 
-    uint64_t elapsed_time_ns  = (current_cycles / frequency) * 1000000000LL;
+    uint64_t elapsed_time_ns  = (current_cycles / frequency)  * 1000000000LL;
     elapsed_time_ns          += ((current_cycles % frequency) * 1000000000LL) / frequency;
 
     return (time_t)elapsed_time_ns;
