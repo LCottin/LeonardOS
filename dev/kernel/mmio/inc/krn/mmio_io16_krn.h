f2243f6d344fb519cf234a35b4f04f9be2c3c56b
LCottin <cottin_leonard@icloud.com>
2026-05-27 17:55:43 +0200

    Split mmio_io16 code file

--- dev/kernel/mmio/inc/krn/mmio_io16_krn.h
+++ dev/kernel/mmio/inc/krn/mmio_io16_krn.h
@@ -30,7 +30,7 @@ extern uint16_t mmio_io16_read(cptr_t addr);
  * @brief Sets a specific bit in an 16-bit memory-mapped I/O register.
  *
  * @param addr The memory address of the I/O register.
- * @param bit The bit position (0-7) to set.
+ * @param bit The bit position (0-15) to set.
  *
  * @return This function does not return a value.
  **********************************************************************/
@@ -41,7 +41,7 @@ extern void mmio_io16_set_bit(ptr_t addr, const uint16_t bit);
  * @brief Clears a specific bit in an 16-bit memory-mapped I/O register.
  *
  * @param addr The memory address of the I/O register.
- * @param bit The bit position (0-7) to clear.
+ * @param bit The bit position (0-15) to clear.
  *
  * @return This function does not return a value.
  **********************************************************************/
@@ -52,7 +52,7 @@ extern void mmio_io16_clear_bit(ptr_t addr, const uint16_t bit);
  * @brief Toggles a specific bit in an 16-bit memory-mapped I/O register.
  *
  * @param addr The memory address of the I/O register.
- * @param bit The bit position (0-7) to toggle.
+ * @param bit The bit position (0-15) to toggle.
  *
  * @return This function does not return a value.
  **********************************************************************/
