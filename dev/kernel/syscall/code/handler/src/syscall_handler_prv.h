c98cdbf4f6a109991e433acb4e158fd20a4ccdcf
LCottin <cottin_leonard@icloud.com>
2026-07-03 10:26:48 +0200

    Update syscall handler with new interface

--- dev/kernel/syscall/code/handler/src/syscall_handler_prv.h
+++ dev/kernel/syscall/code/handler/src/syscall_handler_prv.h
@@ -9,13 +9,10 @@
  *
  * This function performs the corresponding system call function.
  *
- * @param syscall_number The system call number.
- * @param arg1 The first argument for the system call.
- * @param arg2 The second argument for the system call.
- * @param ret The return value of the system call.
+ * @param syscall_request Pointer to the system call request structure.
  * @return None.
  **********************************************************************/
-extern void syscall_manager(const syscall_numbers_e syscall_number, cptr_t arg1, cptr_t arg2, ptr_t ret);
+extern void syscall_manager(syscall_request_t *request);
 
 
 #endif /* __SYSCALL_HANDLER_PRV_H__ */
