03d9c2b94ad2a99c8aa13aa4d099f61b0bab9b40
LCottin <cottin_leonard@icloud.com>
2026-07-10 14:25:50 +0200

    Clean up unused headers

--- dev/kernel/pcb/code/init/inc/pcb_init.h
+++
@@ -1,4 +0,0 @@
-#ifndef __PCB_INIT_H__
-#define __PCB_INIT_H__
-
-#endif /* __PCB_INIT_H__ */
--- dev/kernel/pcb/code/init/src/pcb_init_prv.h
+++
@@ -1,4 +0,0 @@
-#ifndef __PCB_INIT_PRV_H__
-#define __PCB_INIT_PRV_H__
-
-#endif /* __PCB_INIT_PRV_H__ */
